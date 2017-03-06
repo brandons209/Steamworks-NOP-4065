6a8f2042568d82d0cafedfac32f814476a0fea3e
nerds <nerds@DESKTOP-MQHG1AS.centurylink.net>
2017-03-05 23:26:58 -0500

    hi

--- src/Commands/Autonomous Commands/AutoCommandBuilder.cpp
+++ src/Commands/Autonomous Commands/AutoCommandBuilder.cpp
@@ -4,7 +4,7 @@ AutoCommandBuilder::AutoCommandBuilder() {
 
 	if(Robot::autoChooser1.GetSelected() == "drive"){
 		SmartDashboard::PutNumber("Drive to point x:", Robot::driveTrain.get()->realX);
-		SmartDashboard::PutNumber("Drive to point y:", Robot::driveTrain.get()->realY + 96.0);
+		SmartDashboard::PutNumber("Drive to point y:", Robot::driveTrain.get()->realY + (116.0 - Robot::yOffset));
 		AddSequential(new DriveForward());
 	}
 
